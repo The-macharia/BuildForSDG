e9ee3f492739a926a01a058a1e27407c5634314f
Victor Baraka <barakavictor62@gmail.com>
2020-04-10 17:04:46 +0300

    fix(estimate endpoint): set force option of get_json() function to True
    add a extra view to interact with the API from the browser
    add jquery to launch api requests via ajax
    add boostrap styling to the added view

--- covid19estimator/__init__.py
+++ covid19estimator/__init__.py
@@ -8,4 +8,7 @@ def create_app(config='covid19estimator.settings'):
     from .api import api_bp
     app.register_blueprint(api_bp)
 
+    from .main import main_bp
+    app.register_blueprint(main_bp)
+
     return app
--- covid19estimator/api/routes.py
+++ covid19estimator/api/routes.py
@@ -8,7 +8,7 @@ from src.estimator import estimator
 @api_bp.route("/v1/on-covid-19", defaults={"output_format": None}, methods=('POST',))
 @api_bp.route("/v1/on-covid-19/<string:output_format>", methods=('POST',))
 def estimate(output_format):
-    output = estimator(request.get_json())
+    output = estimator(request.get_json(force=True))
     if output_format and output_format == 'xml':
         return Response(json_to_xml(output), mimetype='application/xml')
     return jsonify(output)
@@ -18,7 +18,7 @@ def logging():
     from covid19estimator.settings import BASE_DIR
     logs = ''
     with open('{}/estimator.logs'.format(BASE_DIR), 'r') as log_file:
-        logs = "\n".join(line for line in log_file.readlines())
+        logs = log_file.read()
 
     return logs
     
