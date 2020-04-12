ece0c888e73faf440c748588888a5b5b20a9e3a7
Victor Baraka <barakavictor62@gmail.com>
2020-04-12 14:45:56 +0300

    refactor(estimator): create a folder to cotain helper functions for the main estimator function

--- src/estimator.py
+++ src/estimator.py
@@ -1,56 +1,4 @@
-input_data = {
-  "region": {
-    "name": "Africa",
-    "avgAge": 19.7,
-    "avgDailyIncomeInUSD": 5,
-    "avgDailyIncomePopulation": 0.71
-  },
-  "periodType": "days",
-  "timeToElapse": 58,
-  "reportedCases": 674,
-  "population": 66622705,
-  "totalHospitalBeds": 1380614
-}
-
-def get_infected(reportedCases, factor):
-  infected = reportedCases * factor
-  return infected
-
-def get_infections_by_requested_time(currentlyInfected, days):
-  factor = int((days // 3))
-  infections_after_time_period = currentlyInfected * (2**factor)
-  return infections_after_time_period
-
-def normalise_days(periodType, value):
-  periodType = periodType.lower()
-  normalised_days = 0
-  if periodType == 'weeks':
-    normalised_days = value * 7
-  elif periodType == 'months':
-    normalised_days = value * 30
-  else:
-    normalised_days = value
-  return normalised_days
-
-def get_severe_cases_by_requested_time(value, factor=0.15):
-  cases = factor * value 
-  return cases
-
-def get_available_beds(servereCases, totalHospitalBeds, average_available=0.35):
-  available_beds = int((average_available * totalHospitalBeds) - servereCases)
-  return available_beds
-
-def get_cases_for_ICU(infectionsByRequestedTime, factor=0.05):
-  cases_for_ICU = factor * infectionsByRequestedTime
-  return int(cases_for_ICU)
-
-def get_cases_requiring_ventilators(infectionsByRequestedTime, factor=0.02):
-  cases_requiring_ventilators = factor * infectionsByRequestedTime
-  return int(cases_requiring_ventilators)
-
-def get_dollars_in_flight(infections_by_requested_time, avg_earners, avg_daily_income, days):
-  expected_loss = (infections_by_requested_time * avg_earners * avg_daily_income) // days  
-  return expected_loss
+from helpers.helpers import *
 
 def estimator(data):
   currentlyInfectedImpact = get_infected(data['reportedCases'], 10)
@@ -109,6 +57,7 @@ def estimator(data):
     "dollarsInFlight": dollarsInFlightImpact
 
   }
+
   severeImpact = {
     "currentlyInfected": currentlyInfectedServereImpact,
     "infectionsByRequestedTime": infectionsByRequestedTimeSevereImpact,
